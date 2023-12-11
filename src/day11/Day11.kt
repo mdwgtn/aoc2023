f29427616fc7ccb296b016aee98ec7e40e7a1b6e
Mike Davidson <mikedavidson@Mikes-MacBook-Pro.local>
2023-12-11 23:46:28 +1300

    Add Day 11 Part 2

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -2,16 +2,17 @@ package day11
 
 import readInput
 import testFileName
-import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val day = "Day11"
-    val test01Expected = 374
-    val test02Expected = 2
+    val test01Expected = 374L
+    val test02Expected = 82000210L
 
-    fun part1(input: List<String>): Int = ExpandedGalaxy(input).distanceSum
+    fun part1(input: List<String>): Long = ExpandedGalaxy(input, 2).distanceSum
 
-    fun part2(input: List<String>): Int = test02Expected
+    fun part2(input: List<String>): Long = ExpandedGalaxy(input, 1000000).distanceSum
 
     check(part1(readInput(testFileName(day))) == test01Expected)
     check(part2(readInput(testFileName(day))) == test02Expected)
@@ -20,60 +21,68 @@ fun main() {
     println("Part 2 answer: " + part2(input))
 }
 
-class ExpandedGalaxy(input: List<String>) {
-    private val unexpandedGalaxy = input
-    val expandedGalaxy = expandGalaxy()
+class ExpandedGalaxy(input: List<String>, expansionFactor: Int) {
+    private val galaxy = input
 
-    private fun galaxyMapColumnIsEmpty(col: Int): Boolean = unexpandedGalaxy.all { it[col] == '.'}
-    private fun galaxyMapRowIsEmpty(row: Int): Boolean = unexpandedGalaxy[row].all{ it == '.' }
-    private fun widenGalaxyMapColumn(galaxy: List<String>, col:Int): List<String> = galaxy.map { it.slice(0 until col) + "." + it.slice(col until it.length) }
-    private fun widenGalaxyMapRow(galaxy: List<String>, row: Int): List<String> = galaxy.slice(0 until row) + ".".repeat(galaxy[0].length) + galaxy.slice(row until galaxy.size)
+    private fun galaxyMapColumnIsEmpty(col: Int): Boolean = galaxy.all { it[col] == '.' }
+    private fun galaxyMapRowIsEmpty(row: Int): Boolean = galaxy[row].all { it == '.' }
 
-    private fun emptyGalaxyMapColumns(): List<Int> =  (0 until unexpandedGalaxy[0].length).mapIndexedNotNull{ index, _ -> if (galaxyMapColumnIsEmpty(index)) index else null}
-    private fun emptyGalaxyMapRows(): List<Int> = unexpandedGalaxy.indices.mapIndexedNotNull { index, _ -> if (galaxyMapRowIsEmpty(index)) index else null }
-    private fun expandGalaxy(): List<String> {
-        val emptyColumns = emptyGalaxyMapColumns()
-        val emptyRows = emptyGalaxyMapRows()
-        var result = unexpandedGalaxy
-        for ((doneSoFar, row) in emptyRows.withIndex()) {
-            result = widenGalaxyMapRow(result, row + doneSoFar)
-        }
-        for((doneSoFar, col) in emptyColumns.withIndex()) {
-            result = widenGalaxyMapColumn(result, col + doneSoFar)
-        }
-        println(result)
-        return result
-    }
+    private fun emptyGalaxyMapColumns(): List<Int> =
+        (0 until galaxy[0].length).mapIndexedNotNull { index, _ -> if (galaxyMapColumnIsEmpty(index)) index else null }
 
-    val galaxyIndices = expandedGalaxy.flatMapIndexed { rowIndex, row ->
+    private fun emptyGalaxyMapRows(): List<Int> =
+        galaxy.indices.mapIndexedNotNull { index, _ -> if (galaxyMapRowIsEmpty(index)) index else null }
+
+    private val galaxyIndices = galaxy.flatMapIndexed { rowIndex, row ->
         row.mapIndexedNotNull { colIndex, char ->
             if (char == '#') Pair(rowIndex, colIndex) else null
-        }.map { Location(it.first, it.second)}
+        }.map { Location(it.first, it.second) }
     }
 
-    val distanceSum = PairwiseDistanceSum(galaxyIndices).distanceSum
+    val distanceSum =
+        PairwiseDistanceSum(galaxyIndices, expansionFactor, emptyGalaxyMapRows(), emptyGalaxyMapColumns()).distanceSum
 
 }
 
-class PairwiseDistanceSum(val locations: List<Location>) {
-    val distanceSum = distanceSum()
+class PairwiseDistanceSum(
+    locations: List<Location>,
+    expansionFactor: Int,
+    emptyGalaxyMapRows: List<Int>,
+    emptyGalaxyMapColumns: List<Int>
+) {
+    val distanceSum = distanceSum(locations, expansionFactor, emptyGalaxyMapRows, emptyGalaxyMapColumns)
 
-    private fun distanceSum(): Int {
-        var sum = 0
+    private fun distanceSum(
+        locations: List<Location>,
+        expansionFactor: Int,
+        emptyGalaxyMapRows: List<Int>,
+        emptyGalaxyMapColumns: List<Int>
+    ): Long {
+        var sum = 0L
         for (lhs in 0 until locations.size - 1) {
             for (rhs in lhs + 1 until locations.size) {
-                sum += locations[lhs].distanceFrom(locations[rhs])
+                sum += locations[lhs].distanceFrom(
+                    locations[rhs],
+                    expansionFactor,
+                    emptyGalaxyMapRows,
+                    emptyGalaxyMapColumns
+                )
             }
         }
         return sum
     }
 }
 
-
 class Location(val row: Int, val col: Int) {
-    fun distanceFrom(that: Location): Int  = abs(row - that.row) + abs(col - that.col)
-    override fun toString(): String {
-        return "Location(row=$row, col=$col)"
-    }
+    fun distanceFrom(that: Location, expansionFactor: Int, emptyRows: List<Int>, emptyColumns: List<Int>): Int {
+        val leftColumn: Int = min(col, that.col)
+        val rightColumn: Int = max(col, that.col)
+        val topRow: Int = min(row, that.row)
+        val bottomRow: Int = max(row, that.row)
+        val expandedColumnCount = emptyColumns.intersect(leftColumn..rightColumn).size
+        val expandedRowCount = emptyRows.intersect(topRow..bottomRow).size
 
+        return (rightColumn - leftColumn + (expansionFactor - 1) * expandedColumnCount) +
+                (bottomRow - topRow + (expansionFactor - 1) * expandedRowCount)
+    }
 }
